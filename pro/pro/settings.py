9125c6800d2fa746547bc82c0afc498a012c74f4
Gokul <gokulss.8281@gmail.com>
2023-06-18 00:03:29 +0530

    final

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -127,8 +127,6 @@ STATICFILES_DIRS=[
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
